2a4cec086e1aa2f1f12834b796250e5803ed7c88
manlyeagles <operations@manlyeaglesbaseball.com>
2026-04-17 20:19:43 +1000

    Update server.js

--- server.js
+++ server.js
@@ -7,18 +7,11 @@ const pool = new Pool({
   connectionString: process.env.DATABASE_URL
 });
 
-// health check (so "/" works)
-app.get('/', (req, res) => {
-  res.send('API is running');
-});
-
-// test route
 app.get('/leaderboard/avg', async (req, res) => {
   try {
     const result = await pool.query('SELECT 1');
     res.json(result.rows);
   } catch (err) {
-    console.error('FULL ERROR:', err);
     res.status(500).json(err);
   }
 });
