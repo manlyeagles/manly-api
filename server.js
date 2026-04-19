7ed7ecc15b7ca526bf5839adb8b3c6f385a25d1e
manlyeagles <operations@manlyeaglesbaseball.com>
2026-04-19 17:50:53 +1000

    Update server.js

--- server.js
+++ server.js
@@ -8,10 +8,8 @@ const SUPABASE_KEY = 'sb_publishable_ZG0Uq-sVDa0aFI1zkVHZiw_wBBNYpA4';
 
 app.get('/leaderboard/view', async (req, res) => {
   try {
-    const season = req.query.season || 1;
-
     const response = await fetch(
-      `${SUPABASE_URL}/rest/v1/player_season_stats?season_id=eq.${season}&select=player_id,"AB","H",players(first_name,last_name,grade)`,
+      `${SUPABASE_URL}/rest/v1/player_season_stats?select=player_id,season_id,"AB","H",players(first_name,last_name,grade)`,
       {
         headers: {
           apikey: SUPABASE_KEY,
@@ -36,6 +34,7 @@ app.get('/leaderboard/view', async (req, res) => {
           <td>${i + 1}</td>
           <td>${p.players?.first_name || ''} ${p.players?.last_name || ''}</td>
           <td>${p.players?.grade || ''}</td>
+          <td>${p.season_id}</td>
           <td>${avg}</td>
         </tr>
       `;
@@ -44,13 +43,13 @@ app.get('/leaderboard/view', async (req, res) => {
     res.send(`
       <html>
         <body>
-          <h2>Season ${season}</h2>
           <table border="1" style="width:100%; border-collapse: collapse;">
             <thead>
               <tr>
                 <th>#</th>
                 <th>Name</th>
                 <th>Grade</th>
+                <th>Season</th>
                 <th>AVG</th>
               </tr>
             </thead>
