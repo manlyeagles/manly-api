31d6bb0fa613bc4c89f4e405627589be22ddecd9
manlyeagles <operations@manlyeaglesbaseball.com>
2026-04-19 18:45:23 +1000

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
+const express = require('express');
+const app = express();
+
+const SUPABASE_URL = 'https://rtmzihkxiwiilxytahre.supabase.co';
+const SUPABASE_KEY = 'sb_publishable_ZG0Uq-sVDa0aFI1zkVHZiw_wBBNYpA4';
+
 app.get('/leaderboard/view', async (req, res) => {
   try {
     const stat = req.query.stat || 'AVG';
+    const season = req.query.season || 1;
 
     const response = await fetch(
-      `${SUPABASE_URL}/rest/v1/player_season_stats?select=player_id,season_id,"AB","H","HR","RBI","OPS","AVG",players(first_name,last_name,grade),seasons(season_name)&order=${stat}.desc`,
+      `${SUPABASE_URL}/rest/v1/player_season_stats?season_id=eq.${season}&select=player_id,season_id,"AB","H","HR","RBI","OPS","AVG",players(first_name,last_name,grade),seasons(season_name)&order=${stat}.desc`,
       {
         headers: {
           apikey: SUPABASE_KEY,
@@ -13,6 +20,10 @@ app.get('/leaderboard/view', async (req, res) => {
     );
 
     const data = await response.json();
+
+    if (!Array.isArray(data)) {
+      return res.send(`<pre>${JSON.stringify(data, null, 2)}</pre>`);
+    }
 
     let rows = '';
 
@@ -35,7 +46,7 @@ app.get('/leaderboard/view', async (req, res) => {
       <html>
         <body>
 
-        <select onchange="window.location='?stat=' + this.value">
+        <select onchange="window.location='?stat=' + this.value + '&season=${season}'">
           <option value="AVG">AVG</option>
           <option value="OPS">OPS</option>
           <option value="HR">HR</option>
@@ -65,4 +76,10 @@ app.get('/leaderboard/view', async (req, res) => {
   } catch (err) {
     res.send(err.toString());
   }
+});
+
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+  console.log(`API running on port ${PORT}`);
 });
