c3b354df07caa8884b4e20b00cd0fe840cb0de1c
manlyeagles <operations@manlyeaglesbaseball.com>
2026-04-19 12:20:15 +1000

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const SUPABASE_KEY = 'sb_publishable_ZG0Uq-sVDa0aFI1zkVHZiw_wBBNYpA4';
 app.get('/leaderboard/view', async (req, res) => {
   try {
     const response = await fetch(
-      `${SUPABASE_URL}/rest/v1/player_season_stats?select=player_id,at_bats,hits,players(first_name,last_name,grade)&order=hits.desc`,
+      `${SUPABASE_URL}/rest/v1/player_season_stats?select=player_id,at_bats,hits,players(first_name,last_name,grade)`,
       {
         headers: {
           apikey: SUPABASE_KEY,
@@ -20,6 +20,13 @@ app.get('/leaderboard/view', async (req, res) => {
     );
 
     const data = await response.json();
+
+    // 👇 DEBUG OUTPUT
+    if (!Array.isArray(data)) {
+      return res.send(`
+        <pre>${JSON.stringify(data, null, 2)}</pre>
+      `);
+    }
 
     let rows = '';
 
